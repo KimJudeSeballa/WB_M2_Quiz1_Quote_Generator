73222ddaf3d6a914160c4af21e1fb9f5ca640639
KimJudeSeballa <judeseballa@gmail.com>
2022-01-04 00:07:27 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Appear Button */
+var quoteBtn1 = document.getElementById("quoteBtn1");
+var elon = document.getElementById("elon");
+var quoteBtn2 = document.getElementById("quoteBtn2");
+var quoteBtn3 = document.getElementById("quoteBtn3");
+var quoteBtn4 = document.getElementById("quoteBtn4");
+var quoteBtn5 = document.getElementById("quoteBtn5");
+
+elon.onclick = appearBtn() ;{
+    quoteBtn1.style.visibility = "visible";
+}
+    
+
